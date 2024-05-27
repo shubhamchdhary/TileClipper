c17bebc1e317dbf7898e3bf70233d8ebabab87be
Shubham Chaudhary <49747028+shubhamchdhary@users.noreply.github.com>
2024-05-27 14:38:23 +0530

    Adds copyright text tileClipper.py

--- src/tileClipper.py
+++ src/tileClipper.py
@@ -1,3 +1,24 @@
+############################################################################
+# TileClipper: Lightweight Selection of Regions of Interest from Videos for 
+# Traffic Surveillance                                                      
+# Copyright (C) 2024 Shubham Chaudhary, Aryan Taneja, Anjali Singh,         
+# Purbasha Roy, Sohum Sikdar, Mukulika Maity, Arani Bhattacharya            
+                                                                            
+# This program is free software: you can redistribute it and/or modify      
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###########################################################################
+
+
 #################################################################################################
 # This scipt has main source code for TileClipper which uses cluster based strategy to filter 
 # unwanted tiles. It requires GPAC's MP4Box for tile manipulation. 
